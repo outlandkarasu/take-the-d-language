df8a8587a4fefee3c3f26cbcf71bc45792190108
outlandkarasu <outland.karasu@gmail.com>
2016-03-22 01:05:48 +0900

    組み込み複素数型の例の追加

--- zeta1/source/app.d
+++ zeta1/source/app.d
@@ -1,10 +1,12 @@
 import std.stdio;
-import std.math;
 
 void main()
 {
-    real 充実 = PI;
-    double 非リア = cast(double) PI;
-    writefln("%.19f", 充実);
-    writefln("%.19f", 非リア);
+    ireal 虚実 = 1.0i;
+    writeln(虚実);
+    writeln(虚実^^2.0);
+
+    creal 複実 = 1.0 + 虚実;
+    writeln(複実);
+    writeln(複実^^2.0);
 }
