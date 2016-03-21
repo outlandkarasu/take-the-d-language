b174308b409572ffbe3a11a7a249b0041049bbc3
outlandkarasu <outland.karasu@gmail.com>
2016-03-22 01:04:23 +0900

    realを使用した計算例の追加

--- zeta1/source/app.d
+++ zeta1/source/app.d
@@ -1,8 +1,10 @@
 import std.stdio;
-
-import zeta;
+import std.math;
 
 void main()
 {
-	writeln(testZeta());
+    real 充実 = PI;
+    double 非リア = cast(double) PI;
+    writefln("%.19f", 充実);
+    writefln("%.19f", 非リア);
 }
